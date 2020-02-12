896c35a25fae8eb3632b8162d894eff23930868c
Yadunund <yadunund@openrobotics.org>
2020-02-12 17:52:07 +0800

    Load and unload of can

--- rmf_gazebo_plugins/src/teleport.cpp
+++ rmf_gazebo_plugins/src/teleport.cpp
@@ -17,6 +17,7 @@
 
 #include <gazebo/common/Plugin.hh>
 #include <gazebo/physics/Model.hh>
+#include <gazebo_ros/node.hpp>
 
 #include <ignition/math/Vector3.hh>
 
@@ -35,10 +36,25 @@ public:
   using DispenserResult = rmf_dispenser_msgs::msg::DispenserResult;
   using Pose3d = ignition::math::Pose3d;
 
+  // Pointer to the model
+  gazebo::physics::ModelPtr _model;
+  gazebo_ros::Node::SharedPtr _node;
+  rclcpp::Subscription<DispenserResult>::SharedPtr _result_sub;
+  bool _load_complete = false;
+
+  // The guid of the loading and unloading dispensers
+  std::string _load_guid;
+  std::string _unload_guid;
+
+  Pose3d _load_pose;
+  Pose3d _unload_pose;
+
+  bool _object_loaded = false;
+
   void Load(gazebo::physics::ModelPtr _parent, sdf::ElementPtr _sdf) override
   {
     // Store the pointer to the model
-    this->_model = _parent;
+    _model = _parent;
 
     // Get the required sdf parameters
     get_sdf_param_required<std::string>(_sdf, "load_guid", _load_guid);
@@ -46,30 +62,42 @@ public:
     get_sdf_param_required<Pose3d>(_sdf, "load_pose", _load_pose);
     get_sdf_param_required<Pose3d>(_sdf, "unload_pose", _unload_pose);
 
-    _node = std::make_shared<rclcpp::Node>("teleport_plugin_node");
+    _node = gazebo_ros::Node::Get(_sdf);
     std::cout << "Started teleport_plugin node..." <<std::endl;
 
     _result_sub = _node->create_subscription<DispenserResult>(
         "/dispenser_results",
-        rclcpp::QoS(10),
+        rclcpp::SystemDefaultsQoS(),
         [&](DispenserResult::UniquePtr msg)
         {
           dispenser_result_cb(std::move(msg));
         });
     
-    std::cout << "Sub started..." <<std::endl;
-
     _load_complete = true;
   }
 
   // Called by the world update start event
   void dispenser_result_cb(DispenserResult::UniquePtr msg)
   {
-    if (msg->source_guid == _load_guid && msg->status == 1)
-    {
-      _model->SetWorldPose(_load_pose);
-    }
+    auto status = msg->status;
+    std::string source_guid = msg->source_guid.c_str();
 
+    if (source_guid == _load_guid && !_object_loaded)
+    {
+      RCLCPP_INFO(_node->get_logger(),  "Loading object");
+      _model->SetWorldPose(_load_pose);
+      _object_loaded = true;
+    }
+    else if (source_guid == _unload_guid && _object_loaded)
+    {
+      RCLCPP_INFO(_node->get_logger(),  "Unloading object");
+      _model->SetWorldPose(_unload_pose);
+      _object_loaded = false;
+    }
+    else
+    {
+      return;
+    } 
   }
 
   ~TeleportPlugin()
@@ -79,22 +107,6 @@ public:
       rclcpp::shutdown();
     }
   }
-
-
-  // Pointer to the model
-  gazebo::physics::ModelPtr _model;
-
-  rclcpp::Node::SharedPtr _node;
-  rclcpp::Subscription<DispenserResult>::SharedPtr _result_sub;
-
-  bool _load_complete = false;
-
-  // The guid of the loading and unloading dispensers
-  std::string _load_guid;
-  std::string _unload_guid;
-
-  Pose3d _load_pose;
-  Pose3d _unload_pose;
 };
 
 // Register this plugin with the simulator
