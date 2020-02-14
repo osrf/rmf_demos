70974f89d66e993a6633ba1d66ac5c6209a154dc
Yadunund <yadunund@openrobotics.org>
2020-02-14 12:44:24 +0800

    Created layout

--- rmf_rviz_plugin/src/Control.cpp
+++ rmf_rviz_plugin/src/Control.cpp
@@ -1,1 +1,97 @@
+/*
+ * Copyright (C) 2020 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
 #include "Control.hpp"
+
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <Qlabel>
+
+namespace rmf_rviz_plugin {
+
+RMFPanel::create_layout()
+{
+  // Layout for delivery request
+  // HLayout 1
+  QHBoxLayout* layout_delivery1 = new QHBoxLayout;
+  layout_delivery1->addWidget(new QLabel("Delivery Request"));
+  layout_delivery1->addStretch();
+
+  // HLayout 2
+  QHBoxLayout* layout_delivery2 = new QHBoxLayout;
+  layout_delivery2->addWidget(new QLabel("Task ID:"));
+  _delivery_task_id_editor = new QLineEdit;
+  _delivery_task_id_editor->setFixedWidth(200);
+  layout_delivery2->addWidget(_delivery_task_id_editor);
+
+  layout_delivery2->addWidget(new QLabel("Robot:"));
+  _delivery_robot_editor = new QLineEdit;
+  _delivery_robot_editor->setFixedWidth(200);
+  layout_delivery2->addWidget(_delivery_robot_editor);
+  layout_delivery2->addStretch();
+
+  // HLayout 3
+  QHBoxLayout* layout_delivery3 = new QHBoxLayout;
+  layout_delivery3->addWidget(new QLabel("Pickup:"));
+  _delivery_pickup_editor = new QLineEdit;
+  _delivery_pickup_editor->setFixedWidth(200);
+  layout_delivery3->addWidget(_delivery_pickup_editor);
+
+  layout_delivery3->addWidget(new QLabel("Dropoff:"));
+  _delivery_dropoff_editor = new QLineEdit;
+  _delivery_dropoff_editor->setFixedWidth(200);
+  layout_delivery3->addWidget(_delivery_dropoff_editor);
+  layout_delivery3->addStretch();
+
+  //HLayout 4
+  QHBoxLayout* layout_delivery4 = new QHBoxLayout;
+  _delivery_button = new QPushButton(this);
+  _delivery_button->setText("Request Delivery");
+  layout_delivery4->addWidget(new QLabel("Pickup:"));
+  layout_delivery4->addStretch();
+
+  QVHoxLayout* layout_delivery = new QVBoxLayout;
+  layout_delivery->addStretch();
+  layout_delivery->addLayout(layout_delivery1)
+  layout_delivery->addLayout(layout_delivery2)
+  layout_delivery->addLayout(layout_delivery3)
+  layout_delivery->addLayout(layout_delivery4)
+  layout_delivery->addStretch();
+}
+
+RMFPanel::RMFPanel(QWidget* parent)
+: rviz_common::Panel(parent)
+{
+  _node = std::make_shared<rclcpp::Node>("rmf_panel_plugin");
+
+  _delivery_pub = _node->create_publisher<Delivery>(
+    "/delivery_requests",
+    rclcpp::QoS(10));
+
+  // Initialize labels
+  _delivery_task_id->setText("");
+  _delivery_pickup->setText("pantry");
+  _delivery_dropoff->setText("hardware_2");
+  _delivery_robot->setText("magni");
+
+  create_layout();
+}
+
+} // namespace rmf_rviz_plugin
+
+#include <pluginlib/class_list_macros.hpp>
+PLUGINLIB_EXPORT_CLASS(rmf_rviz_plugin::RMFPanel, rviz_common::Panel)
--- rmf_rviz_plugin/src/Control.hpp
+++ rmf_rviz_plugin/src/Control.hpp
@@ -1,5 +1,81 @@
-#ifndef SRC_CONTROL_HPP
-#define SRC_CONTROL_HPP
+/*
+ * Copyright (C) 2020 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
 
+#ifndef RMF_RVIZ_PLUGIN_CONTROL_HPP
+#define RMF_RVIZ_PLUGIN_CONTROL_HPP
 
-#endif // SRC_CONTROL_HPP
+#include <rviz_common/panel.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+#include <rmf_task_msgs/msg/delivery.msg>
+
+#include <QLineEdit>
+#include <QPushButton>
+
+#include <memory>
+
+namesapce rmf_rviz_plugin {
+
+using Delivery = rmf_task_msgs::msg::Delivery;
+
+class RMFPanel : public rviz_common::Panel
+{
+Q_OBJECT
+public:
+  RMFPanel(QWidget* parent =0);
+  ~RMFPanel();
+
+  virtual void load(const rviz_common::Config& config);
+  virtual void save(rviz_common::Config config) const;
+
+public Q_SLOTS:
+  void set_delivery_task_id(const QString& max);
+  void set_delivery_pickup(const QString& topic);
+  void set_delivery_dropoff(const QString& map_name);
+  void set_delivery_robot(const QString& map_name);
+
+protected Q_SLOTS:
+  void update_delivery_task_id();
+  void update_delivery_pickup();
+  void update_delivery_dropoff();
+  void update_delivery_robot();
+  void request_delivery();
+
+protected:
+  
+  void create_layout();
+
+  QLineEdit* _delivery_task_id_editor;
+  QLineEdit* _delivery_pickup_editor;
+  QLineEdit* _delivery_dropoff_editor;
+  QLineEdit* _delivery_robot_editor;
+
+  QPushButton* _delivery_button;
+
+  QString _delivery_task_id;
+  QString _delivery_pickup;
+  QString _delivery_dropoff;
+  QString _delivery_robot;
+
+  rclcpp::Node::SharedPtr _node;
+  rclcpp::Publisher<Delivery>::SharedPtr _delivery_pub;
+
+};
+
+} // namespace rmf_rviz_plugin
+
+#endif // RMF_RVIZ_PLUGIN_CONTROL_HPP
