35f38fbc011110f475ef10f9695170cfd4936814
Yadunund <yadunund@openrobotics.org>
2020-02-17 10:58:40 +0800

    Edited layouts

--- rmf_rviz_plugin/src/RmfPanel.cpp
+++ rmf_rviz_plugin/src/RmfPanel.cpp
@@ -21,6 +21,7 @@
 #include <QVBoxLayout>
 #include <QHBoxLayout>
 #include <QLabel>
+#include <QGroupBox>
 
 namespace rmf_rviz_plugin {
 
@@ -29,114 +30,110 @@ void RmfPanel::create_layout()
   // Layout for delivery request
   // HLayout 1
   QHBoxLayout* layout_delivery1 = new QHBoxLayout;
-  layout_delivery1->addWidget(new QLabel("Delivery Request"));
+  layout_delivery1->addWidget(new QLabel("Task ID:"));
+  _delivery_task_id_editor = new QLineEdit;
+  _delivery_task_id_editor->setFixedWidth(150);
+  layout_delivery1->addWidget(_delivery_task_id_editor);
+
+  layout_delivery1->addWidget(new QLabel("Robot:"));
+  _delivery_robot_editor = new QLineEdit;
+  _delivery_robot_editor->setFixedWidth(150);
+  layout_delivery1->addWidget(_delivery_robot_editor);
   layout_delivery1->addStretch();
 
   // HLayout 2
   QHBoxLayout* layout_delivery2 = new QHBoxLayout;
-  layout_delivery2->addWidget(new QLabel("Task ID:"));
-  _delivery_task_id_editor = new QLineEdit;
-  _delivery_task_id_editor->setFixedWidth(200);
-  layout_delivery2->addWidget(_delivery_task_id_editor);
+  layout_delivery2->addWidget(new QLabel("Pickup:"));
+  _delivery_pickup_editor = new QLineEdit;
+  _delivery_pickup_editor->setFixedWidth(150);
+  layout_delivery2->addWidget(_delivery_pickup_editor);
 
-  layout_delivery2->addWidget(new QLabel("Robot:"));
-  _delivery_robot_editor = new QLineEdit;
-  _delivery_robot_editor->setFixedWidth(200);
-  layout_delivery2->addWidget(_delivery_robot_editor);
+  layout_delivery2->addWidget(new QLabel("Dropoff:"));
+  _delivery_dropoff_editor = new QLineEdit;
+  _delivery_dropoff_editor->setFixedWidth(150);
+  layout_delivery2->addWidget(_delivery_dropoff_editor);
   layout_delivery2->addStretch();
 
-  // HLayout 3
+  //HLayout 3
   QHBoxLayout* layout_delivery3 = new QHBoxLayout;
-  layout_delivery3->addWidget(new QLabel("Pickup:"));
-  _delivery_pickup_editor = new QLineEdit;
-  _delivery_pickup_editor->setFixedWidth(200);
-  layout_delivery3->addWidget(_delivery_pickup_editor);
-
-  layout_delivery3->addWidget(new QLabel("Dropoff:"));
-  _delivery_dropoff_editor = new QLineEdit;
-  _delivery_dropoff_editor->setFixedWidth(200);
-  layout_delivery3->addWidget(_delivery_dropoff_editor);
-  layout_delivery3->addStretch();
-
-  //HLayout 4
-  QHBoxLayout* layout_delivery4 = new QHBoxLayout;
   _delivery_button = new QPushButton(this);
   _delivery_button->setText("Request Delivery");
-  layout_delivery4->addWidget(_delivery_button);
-  layout_delivery4->addStretch();
+  layout_delivery3->addWidget(_delivery_button);
+  layout_delivery3->addStretch();
 
   QVBoxLayout* layout_delivery = new QVBoxLayout;
   layout_delivery->addLayout(layout_delivery1);
   layout_delivery->addLayout(layout_delivery2);
   layout_delivery->addLayout(layout_delivery3);
-  layout_delivery->addLayout(layout_delivery4);
+
+  auto delivery_box = new QGroupBox("Delivery Request");
+  delivery_box->setLayout(layout_delivery);
 
   // Layout for loop request
   // HLayout 1
-  QHBoxLayout* layout1 = new QHBoxLayout;
-  layout1->addWidget(new QLabel("Loop Request"));
-  layout1->addStretch();
+  QHBoxLayout* layout_loop1 = new QHBoxLayout;
+  layout_loop1->addWidget(new QLabel("Task ID:"));
+  _loop_task_id_editor = new QLineEdit;
+  _loop_task_id_editor->setFixedWidth(150);
+  layout_loop1->addWidget(_loop_task_id_editor);
+
+  layout_loop1->addWidget(new QLabel("Robot:"));
+  _loop_robot_editor = new QLineEdit;
+  _loop_robot_editor->setFixedWidth(150);
+  layout_loop1->addWidget(_loop_robot_editor);
+  layout_loop1->addStretch();
 
   // HLayout 2
-  QHBoxLayout* layout2 = new QHBoxLayout;
-  layout2->addWidget(new QLabel("Task ID:"));
-  _loop_task_id_editor = new QLineEdit;
-  _loop_task_id_editor->setFixedWidth(100);
-  layout2->addWidget(_loop_task_id_editor);
-
-  layout2->addWidget(new QLabel("Robot:"));
-  _loop_robot_editor = new QLineEdit;
-  _loop_robot_editor->setFixedWidth(100);
-  layout2->addWidget(_loop_robot_editor);
-  layout2->addStretch();
-
-  // HLayout 3
-  QHBoxLayout* layout3 = new QHBoxLayout;
-  layout3->addWidget(new QLabel("Start:"));
+  QHBoxLayout* layout_loop2 = new QHBoxLayout;
+  layout_loop2->addWidget(new QLabel("Start:"));
   _loop_start_editor = new QLineEdit;
-  _loop_start_editor->setFixedWidth(100);
-  layout3->addWidget(_loop_start_editor);
+  _loop_start_editor->setFixedWidth(150);
+  layout_loop2->addWidget(_loop_start_editor);
 
-  layout3->addWidget(new QLabel("Finish:"));
+  layout_loop2->addWidget(new QLabel("Finish:"));
   _loop_finish_editor = new QLineEdit;
-  _loop_finish_editor->setFixedWidth(100);
-  layout3->addWidget(_loop_finish_editor);
-  layout3->addStretch();
+  _loop_finish_editor->setFixedWidth(150);
+  layout_loop2->addWidget(_loop_finish_editor);
+  layout_loop2->addStretch();
 
-  //HLayout 4
-  QHBoxLayout* layout4 = new QHBoxLayout;
+  //HLayout 3
+  QHBoxLayout* layout_loop3 = new QHBoxLayout;
+  layout_loop3->addWidget(new QLabel("# Loops:"));
   _loop_num_editor = new QLineEdit;
-  _loop_num_editor->setFixedWidth(100);
-  layout4->addWidget(_loop_num_editor);
+  _loop_num_editor->setFixedWidth(150);
+  layout_loop3->addWidget(_loop_num_editor);
 
   _loop_button = new QPushButton(this);
   _loop_button->setText("Request Loop");
-  layout4->addWidget(_loop_button);
-  layout4->addStretch();
+  layout_loop3->addWidget(_loop_button);
+  layout_loop3->addStretch();
 
   QVBoxLayout* layout_loop = new QVBoxLayout;
-  layout_loop->addLayout(layout1);
-  layout_loop->addLayout(layout2);
-  layout_loop->addLayout(layout3);
-  layout_loop->addLayout(layout4);
+  layout_loop->addLayout(layout_loop1);
+  layout_loop->addLayout(layout_loop2);
+  layout_loop->addLayout(layout_loop3);
 
+  auto loop_box = new QGroupBox("Loop Request");
+  loop_box->setLayout(layout_loop);
+
+  // Combining layouts into a single vertical layout
   QVBoxLayout* layout = new QVBoxLayout;
   layout->addStretch();
-  layout->addLayout(layout_delivery);
-  layout->addLayout(layout_loop);
+  layout->addWidget(loop_box);
+  layout->addWidget(delivery_box);
   setLayout(layout);
 
   // Initialize text fields
+  _delivery_task_id_editor->setText(_delivery_task_id);
+  _delivery_pickup_editor->setText(_delivery_pickup);
+  _delivery_dropoff_editor->setText(_delivery_dropoff);
+  _delivery_robot_editor->setText(_delivery_robot);
+
   _loop_task_id_editor->setText(_loop_task_id);
   _loop_start_editor->setText(_loop_start);
   _loop_finish_editor->setText(_loop_finish);
   _loop_robot_editor->setText(_loop_robot);
   _loop_num_editor->setText(_loop_num);
-
-  _delivery_task_id_editor->setText(_delivery_task_id);
-  _delivery_pickup_editor->setText(_delivery_pickup);
-  _delivery_dropoff_editor->setText(_delivery_dropoff);
-  _delivery_robot_editor->setText(_delivery_robot);
 
   // QT signal connections
   connect(_delivery_task_id_editor,
@@ -167,11 +164,11 @@ void RmfPanel::create_layout()
 RmfPanel::RmfPanel(QWidget* parent)
 : rviz_common::Panel(parent),
   _delivery_task_id("task#42"),
-  _delivery_robot("Magni"),
+  _delivery_robot("magni"),
   _delivery_pickup("pantry"),
   _delivery_dropoff("hardware_2"),
   _loop_task_id("task#24"),
-  _loop_robot("Magni"),
+  _loop_robot("magni"),
   _loop_num("1"),
   _loop_start("coe"),
   _loop_finish("cubicle_1")
@@ -188,8 +185,6 @@ RmfPanel::RmfPanel(QWidget* parent)
 
   _thread = std::thread([&](){rclcpp::spin(_node);});
   
-  //  create_delivery_layout();
-  //  create_loop_layout();
   create_layout();
   _has_loaded = true;
 }
@@ -203,6 +198,7 @@ RmfPanel::~RmfPanel()
   }
 }
 
+// Delivery set functions 
 void RmfPanel::set_delivery_task_id(const QString& value)
 {
   std::lock_guard<std::mutex> lock(_mutex);
@@ -231,6 +227,43 @@ void RmfPanel::set_delivery_robot(const QString& value)
   Q_EMIT configChanged();
 }
 
+// Loop set functions
+void RmfPanel::set_loop_task_id(const QString& value)
+{
+  std::lock_guard<std::mutex> lock(_mutex);
+  _loop_task_id = value;
+  Q_EMIT configChanged();
+}
+
+void RmfPanel::set_loop_start(const QString& value)
+{
+  std::lock_guard<std::mutex> lock(_mutex);
+  _loop_start = value;
+  Q_EMIT configChanged();
+}
+
+void RmfPanel::set_loop_finish(const QString& value)
+{
+  std::lock_guard<std::mutex> lock(_mutex);
+  _loop_finish = value;
+  Q_EMIT configChanged();
+}
+
+void RmfPanel::set_loop_robot(const QString& value)
+{
+  std::lock_guard<std::mutex> lock(_mutex);
+  _loop_robot = value;
+  Q_EMIT configChanged();
+}
+
+void RmfPanel::set_loop_num(const QString& value)
+{
+  std::lock_guard<std::mutex> lock(_mutex);
+  _loop_num = value;
+  Q_EMIT configChanged();
+}
+
+// Delivery update functions 
 void RmfPanel::update_delivery_task_id()
 {
   set_delivery_task_id(_delivery_task_id_editor->text());
@@ -251,6 +284,33 @@ void RmfPanel::update_delivery_robot()
   set_delivery_robot(_delivery_robot_editor->text());
 }
 
+// Loop update functions
+void RmfPanel::update_loop_task_id()
+{
+  set_loop_task_id(_loop_task_id_editor->text());
+}
+
+void RmfPanel::update_loop_start()
+{
+  set_loop_start(_loop_start_editor->text());
+}
+
+void RmfPanel::update_loop_finish()
+{
+  set_loop_finish(_loop_finish_editor->text());
+}
+
+void RmfPanel::update_loop_robot()
+{
+  set_loop_robot(_loop_robot_editor->text());
+}
+
+void RmfPanel::update_loop_num()
+{
+  set_loop_num(_loop_num_editor->text());
+}
+
+// Publisher functions
 void RmfPanel::request_delivery()
 {
   Delivery delivery;
@@ -283,6 +343,7 @@ void RmfPanel::request_loop()
   RCLCPP_INFO(_node->get_logger(), "Published loop request");
 }
 
+// Load config
 void RmfPanel::load(const rviz_common::Config& config)
 {
   rviz_common::Panel::load(config);
@@ -298,6 +359,7 @@ void RmfPanel::load(const rviz_common::Config& config)
   }
 }
 
+// Save config
 void RmfPanel::save(rviz_common::Config config) const
 {
   rviz_common::Panel::save(config);
