ba45d2973d00119088c270ec692ce90c464caff7
Aaron Chong <aaronchongth@gmail.com>
2020-03-26 17:50:53 +0800

    added scripts and basic config file to download fuel models

---
+++ rmf_demo_assets/scripts/download_fuel_models.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+  echo "usage: ./download_fuel_models.bash TXT_FILE"
+  echo "  where TXT_FILE is a list of Ignition Fuel URLs"
+  exit
+fi
+
+INPUT_FILE=`pwd`/$1
+echo "Downloading Ignition Models listed on file: [$INPUT_FILE]"
+
+NUM_MODELS=$(cat $INPUT_FILE | wc -l)
+echo "Found [$NUM_MODELS] models"
+
+while IFS= read -r line
+do
+  ign fuel download -u "$line" -v 4
+  echo "----------------------------------------"
+done < $INPUT_FILE
