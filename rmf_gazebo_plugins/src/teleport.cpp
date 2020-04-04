6da1bba05bf0e11303b2206f01a9df085c15c523
Aaron Chong <aaronchongth@gmail.com>
2020-04-04 21:39:10 +0800

    Feature/teleport plugin v2 (#62)
    * experimenting with placeholder links on models, refactoring teleport to have dispenser stuff
    
    * no need for mutexes
    
    * fixed RobotPlaceholder model, teleport plugin works, now v3 to remove target name
    
    * extended respawn to 5 seconds, caching modelptrs instead of names
    
    * handle duplicate dispenser request guids
    
    * cleaned up dispenser script and usage
    
    * removed launch call for dispensers

--- rmf_gazebo_plugins/src/teleport.cpp
+++ rmf_gazebo_plugins/src/teleport.cpp
@@ -15,8 +15,13 @@
  *
 */
 
+#include <mutex>
+#include <unordered_map>
+#include <unordered_set>
+
 #include <gazebo/common/Plugin.hh>
 #include <gazebo/physics/Model.hh>
+#include <gazebo/physics/World.hh>
 #include <gazebo_ros/node.hpp>
 
 #include <ignition/math/Vector3.hh>
@@ -25,7 +30,10 @@
 
 #include <rclcpp/rclcpp.hpp>
 
+#include <rmf_fleet_msgs/msg/fleet_state.hpp>
+#include <rmf_dispenser_msgs/msg/dispenser_state.hpp>
 #include <rmf_dispenser_msgs/msg/dispenser_result.hpp>
+#include <rmf_dispenser_msgs/msg/dispenser_request.hpp>
 
 namespace rmf_gazebo_plugins {
 
@@ -33,22 +41,41 @@ class TeleportPlugin : public gazebo::ModelPlugin
 {
 public: 
 
+  gazebo::event::ConnectionPtr _update_connection;
+
+  using FleetState = rmf_fleet_msgs::msg::FleetState;
+  using DispenserState = rmf_dispenser_msgs::msg::DispenserState;
+  using DispenserRequest = rmf_dispenser_msgs::msg::DispenserRequest;
   using DispenserResult = rmf_dispenser_msgs::msg::DispenserResult;
   using Pose3d = ignition::math::Pose3d;
 
   // Pointer to the model
   gazebo::physics::ModelPtr _model;
+  gazebo::physics::WorldPtr _world;
   gazebo_ros::Node::SharedPtr _node;
-  rclcpp::Subscription<DispenserResult>::SharedPtr _result_sub;
+  rclcpp::Subscription<FleetState>::SharedPtr _fleet_state_sub;
+  rclcpp::Publisher<DispenserState>::SharedPtr _state_pub;
+  rclcpp::Subscription<DispenserRequest>::SharedPtr _request_sub;
+  rclcpp::Publisher<DispenserResult>::SharedPtr _result_pub;
+
+  std::unordered_map<std::string, FleetState::UniquePtr> _fleet_states;
+
+  std::unordered_set<std::string> _load_request_guids;
+  std::unordered_set<std::string> _unload_request_guids;
+
+  DispenserState _load_dispenser_state;
+  DispenserState _unload_dispenser_state;
+
+  double _last_pub_time = 0.0;
+
   bool _load_complete = false;
 
   // The guid of the loading and unloading dispensers
   std::string _load_guid;
   std::string _unload_guid;
+  std::vector<gazebo::physics::ModelPtr> _unload_models;
 
   Pose3d _initial_pose;
-  Pose3d _load_pose;
-  Pose3d _unload_pose;
 
   bool _object_loaded = false;
 
@@ -56,57 +83,216 @@ public:
   {
     // Store the pointer to the model
     _model = _parent;
-
-    // Get the required sdf parameters
-    get_sdf_param_required<std::string>(_sdf, "load_guid", _load_guid);
-    get_sdf_param_required<std::string>(_sdf, "unload_guid", _unload_guid);
-    get_sdf_param_required<Pose3d>(_sdf, "load_pose", _load_pose);
-    get_sdf_param_required<Pose3d>(_sdf, "unload_pose", _unload_pose);
+    _world = _model->GetWorld();
 
     _node = gazebo_ros::Node::Get(_sdf);
     std::cout << "Started teleport_plugin node..." <<std::endl;
 
-    _result_sub = _node->create_subscription<DispenserResult>(
-        "/dispenser_results",
-        rclcpp::SystemDefaultsQoS(),
-        [&](DispenserResult::UniquePtr msg)
-        {
-          dispenser_result_cb(std::move(msg));
-        });
+    // Get the required sdf parameters
+    get_sdf_param_required<std::string>(_sdf, "load_guid", _load_guid);
+    get_sdf_param_required<std::string>(_sdf, "unload_guid", _unload_guid);
     
+    std::string prefix = "RobotPlaceholder";
+    get_sdf_param_if_available<std::string>(
+        _sdf, "unload_model_prefix", prefix);
+
+    // Get all the RobotPlaceholder model names
+    auto model_list = _world->Models();
+    for (const auto& m : model_list)
+    {
+      std::string m_name = m->GetName();
+      if (std::mismatch(prefix.begin(), prefix.end(), 
+          m_name.begin(), m_name.end()).first == prefix.end())
+      {
+        _unload_models.push_back(m);
+        RCLCPP_INFO(
+            _node->get_logger(), "Added unloading model: [%s]", 
+            m_name.c_str());
+      }
+    }
+
+    // All the ROS items
+    _fleet_state_sub = _node->create_subscription<FleetState>(
+        "/fleet_states",
+        rclcpp::SystemDefaultsQoS(),
+        [&](FleetState::UniquePtr msg)
+        {
+          fleet_state_cb(std::move(msg));
+        });
+
+    _state_pub = _node->create_publisher<DispenserState>(
+        "/dispenser_states", 10);
+
+    _request_sub = _node->create_subscription<DispenserRequest>(
+        "/dispenser_requests",
+        rclcpp::SystemDefaultsQoS(),
+        [&](DispenserRequest::UniquePtr msg)
+        {
+          dispenser_request_cb(std::move(msg));
+        });
+
+    _result_pub = _node->create_publisher<DispenserResult>(
+      "/dispenser_results", 10);
+    
+    _load_dispenser_state.guid = _load_guid;
+    _load_dispenser_state.mode = DispenserState::IDLE;
+    _unload_dispenser_state.guid = _unload_guid;
+    _unload_dispenser_state.mode = DispenserState::IDLE;
+
+    _update_connection = gazebo::event::Events::ConnectWorldUpdateBegin(
+        std::bind(&TeleportPlugin::on_update, this));
+
     // Fixed location for "reloading" payloads
     _initial_pose = _model->WorldPose();
     _load_complete = true;
   }
 
-  // Called by the world update start event
-  void dispenser_result_cb(DispenserResult::UniquePtr msg)
+  void fleet_state_cb(FleetState::UniquePtr msg)
   {
-    auto status = msg->status;
-    std::string source_guid = msg->source_guid.c_str();
+    _fleet_states[msg->name] = std::move(msg);
+  }
 
-    if (source_guid == _load_guid && !_object_loaded)
+  void dispenser_request_cb(DispenserRequest::UniquePtr msg)
+  {
+    // TODO: the message field should use fleet name instead
+    auto transporter_type = msg->transporter_type;
+    auto dispenser_guid = msg->target_guid;
+    auto request_guid = msg->request_guid;
+    
+    DispenserResult response;
+    response.request_guid = msg->request_guid;
+
+    if (dispenser_guid == _load_guid && !_object_loaded)
     {
-      RCLCPP_INFO(_node->get_logger(),  "Loading object");
-      _model->SetWorldPose(_load_pose);
+      if (!_load_request_guids.insert(request_guid).second)
+        return;
+
+      response.time = _node->now();
+      response.source_guid = _load_guid;
+      response.status = DispenserResult::ACKNOWLEDGED;
+      _result_pub->publish(response);
+
+      RCLCPP_INFO(_node->get_logger(), "Loading object");
+      load_on_nearest_robot(transporter_type);
       _object_loaded = true;
+
+      response.time = _node->now();
+      response.status = DispenserResult::SUCCESS;
+      _result_pub->publish(response);
     }
-    else if (source_guid == _unload_guid && _object_loaded)
+    else if (dispenser_guid == _unload_guid && _object_loaded)
     {
-      RCLCPP_INFO(_node->get_logger(),  "Unloading object");
-      _model->SetWorldPose(_unload_pose);
+      if (!_unload_request_guids.insert(request_guid).second)
+        return;
+
+      response.time = _node->now();
+      response.source_guid = _unload_guid;
+      response.status = DispenserResult::ACKNOWLEDGED;
+      _result_pub->publish(response);
+
+      RCLCPP_INFO(_node->get_logger(), "Unloading object");
+      unload_on_nearest_target();
       _object_loaded = false;
 
-      // Hard coded: Leave object at goal location for 2.0 second, then
+      response.time = _node->now();
+      response.status = DispenserResult::SUCCESS;
+      _result_pub->publish(response);
+      
+      // TODO(Aaron): do this in a separate thread so state publishing continues
+      // Hard coded: Leave object at goal location for 5.0 second, then
       // teleport it back to initial ( pre pickup  ) location
-      rclcpp::sleep_for(std::chrono::seconds(2));
+      rclcpp::sleep_for(std::chrono::seconds(5));
       _model->SetWorldPose(_initial_pose);
       _object_loaded = false;
     }
-    else
+  }
+
+  bool find_nearest_model_name(
+      const std::vector<gazebo::physics::ModelPtr>& models,
+      std::string& nearest_model_name)
+  {
+    double nearest_dist = 1e6;
+    bool found = false;
+
+    for (const auto& m : models)
     {
+      if (!m)
+        continue;
+      
+      double dist =
+          m->WorldPose().Pos().Distance(_model->WorldPose().Pos());
+      if (dist < nearest_dist)
+      {
+        nearest_dist = dist;
+        nearest_model_name = m->GetName();
+        found = true;
+      }
+    }
+
+    return found;
+  }
+
+  void load_on_nearest_robot(const std::string& fleet_name)
+  {    
+    auto fleet_state_it = _fleet_states.find(fleet_name);
+    if (fleet_state_it == _fleet_states.end())
+    {
+      RCLCPP_WARN(_node->get_logger(), 
+          "No such fleet: [%s]", fleet_name.c_str());
       return;
-    } 
+    }
+
+    std::vector<gazebo::physics::ModelPtr> robot_models;
+    for (const auto& rs : fleet_state_it->second->robots)
+    {
+      auto r_model = _world->ModelByName(rs.name);
+      if (r_model)
+        robot_models.push_back(r_model);
+    }
+
+    std::string nearest_robot_model_name;
+    if (!find_nearest_model_name(robot_models, nearest_robot_model_name))
+    {
+      RCLCPP_WARN(_node->get_logger(),
+          "No near robots of fleet [%s] found.", fleet_name.c_str());
+      return;
+    }
+    _model->PlaceOnEntity(nearest_robot_model_name);
+  }
+
+  void unload_on_nearest_target()
+  {
+    std::string nearest_model_name;
+    if (!find_nearest_model_name(_unload_models, nearest_model_name))
+    {
+      RCLCPP_WARN(_node->get_logger(), "No near unloading model found.");
+      return;
+    }
+    _model->SetWorldPose(_world->ModelByName(nearest_model_name)->WorldPose());
+  }
+
+  void on_update()
+  {
+    if (!_load_complete)
+      return;
+
+    const double t = _model->GetWorld()->SimTime().Double();
+    if (t - _last_pub_time >= 2.0)
+    {
+      _last_pub_time = t;
+
+      _load_dispenser_state.time = _node->now();
+      _load_dispenser_state.mode = 
+          _load_dispenser_state.request_guid_queue.empty() ?
+          DispenserState::IDLE : DispenserState::BUSY;
+      _state_pub->publish(_load_dispenser_state);
+
+      _unload_dispenser_state.time = _node->now();
+      _unload_dispenser_state.mode =
+          _unload_dispenser_state.request_guid_queue.empty() ?
+          DispenserState::IDLE : DispenserState::BUSY;
+      _state_pub->publish(_unload_dispenser_state);
+    }
   }
 
   ~TeleportPlugin()
