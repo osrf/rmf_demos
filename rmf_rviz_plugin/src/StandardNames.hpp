2419cfd896654d9e5042db53f7a34f03b2bee20e
Charayaphan Nakorn Boon Han <cnboonhan94@gmail.com>
2020-04-08 13:50:28 +0800

    removing unused topic names

--- rmf_rviz_plugin/src/StandardNames.hpp
+++ rmf_rviz_plugin/src/StandardNames.hpp
@@ -43,10 +43,6 @@ const std::string DeliveryTopicName = "delivery_requests";
 const std::string LoopRequestTopicName = "loop_requests";
 const std::string TaskSummaryTopicName = "task_summaries";
 
-const std::string NavGoalTopicName = "goal_pose";
-const std::string ClickedPointTopicName = "clicked_point";
-const std::string WaypointSelectorTopicName = "initial_pose";
-
 const std::string EmergencyStateTopicName = "fire_alarm_trigger";
 } // namespace rmf_rviz_plugin
 
