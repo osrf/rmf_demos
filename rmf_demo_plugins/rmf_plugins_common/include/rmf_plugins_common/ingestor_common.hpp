a048d1eb0c78a6d483e7f2534f217d225789598e
Rushyendra Maganty <mrushyendra@yahoo.com.sg>
2020-09-23 10:11:28 +0800

    Create Ignition Readonly plugin (#134)
    * Create functional Readonly Ignition plugin and replace map placeholders
    
    New Readonly Ignition plugin mimics the existing Gazebo readonly
    plugin in terms of behavior. robot_placeholder(s) in the Airport
    Terminal map have been replaced with the actual robots themselves.
    
    * Refactor Readonly Gazebo plugin to use ReadonlyCommon class
    
    Moves all non simulator specific code to the ReadOnlyCommon class for
    reuse by other simulator plugins. Refactor the existing Gazebo Readonly
    plugin to make use of this common class.
    
    * Refactor Readonly Ignition plugin to make use of ReadonlyCommon class
    
    Move non Ignition Gazebo specific code to the common class. Amend
    CmakeLists file to remove redundant includes.
    
    * Fix airport demo launch files
    
    Edit description for ignition airport demo launch files, and set default
    caddy model plugin name in sdf file to point to the Gazebo plugin.
    
    * Enable both differential drive plugins to work without needing to recompile
    
    - Add both diff drive plugins to sdf file. Serves as a workaround to run both
    Ignition and Gazebo classic simulations without needing to edit the
    Caddy sdf file to modify plugin names.
    
    - Convert CMake target_link_libraries to ament_target_directories. Remove
    comment markers around caddy in launch file.
    
    * Remove ignition math dependency and substitute Eigen library
    
    Removes use of ignition math in ReadonlyCommon, by replacing it with
    equivalent Eigen types and functions. Added minor comments in the Caddy
    sdf file describing addition of both plugins, as well as some in the
    readonly plugin.
    
    * Miscellaneous minor Readonly plugin improvements
    
    - Pass pose and sim time as arguments to `on_update` function to ensure
    they get computed each time
    - Removed unused `update_count` variable
    - Added getter/setter for name variable in `ReadonlyCommon` and changed
    its access specifier from public to private
    - Modified mutex name to be more descriptive
    
    * Remove _pose and _sim_time local variables from Readonly plugin classes
    
    Initialize the aforementioned variables in the `on_update()` function
    instead.

--- rmf_demo_plugins/rmf_plugins_common/include/rmf_plugins_common/ingestor_common.hpp
+++ rmf_demo_plugins/rmf_plugins_common/include/rmf_plugins_common/ingestor_common.hpp
@@ -97,62 +97,6 @@ private:
     const std::string& fleet_name);
 };
 
-// Ignition math version agnostic conversion functions
-template<typename IgnQuatT>
-inline void convert(const Eigen::Quaterniond& _q, IgnQuatT& quat)
-{
-  quat.W() = _q.w();
-  quat.X() = _q.x();
-  quat.Y() = _q.y();
-  quat.Z() = _q.z();
-}
-
-template<typename IgnVec3T>
-inline void convert(const Eigen::Vector3d& _v, IgnVec3T& vec)
-{
-  vec.X() = _v[0];
-  vec.Y() = _v[1];
-  vec.Z() = _v[2];
-}
-
-template<typename IgnVec3T>
-inline Eigen::Vector3d convert_vec(const IgnVec3T& _v)
-{
-  return Eigen::Vector3d(_v[0], _v[1], _v[2]);
-}
-
-template<typename IgnQuatT>
-inline Eigen::Quaterniond convert_quat(const IgnQuatT& _q)
-{
-  Eigen::Quaterniond quat;
-  quat.w() = _q.W();
-  quat.x() = _q.X();
-  quat.y() = _q.Y();
-  quat.z() = _q.Z();
-
-  return quat;
-}
-
-template<typename IgnPoseT>
-inline auto convert_to_pose(const Eigen::Isometry3d& _tf)
-{
-  IgnPoseT pose;
-  convert(Eigen::Vector3d(_tf.translation()), pose.Pos());
-  convert(Eigen::Quaterniond(_tf.linear()), pose.Rot());
-
-  return pose;
-}
-
-template<typename IgnPoseT>
-inline Eigen::Isometry3d convert_pose(const IgnPoseT& _pose)
-{
-  Eigen::Isometry3d tf = Eigen::Isometry3d::Identity();
-  tf.translation() = convert_vec(_pose.Pos());
-  tf.linear() = Eigen::Matrix3d(convert_quat(_pose.Rot()));
-
-  return tf;
-}
-
 } // namespace rmf_ingestor_common
 
 #endif
---
+++ rmf_demo_plugins/rmf_plugins_common/include/rmf_plugins_common/readonly_common.hpp
@@ -0,0 +1,161 @@
+/*
+ * Copyright (C) 2020 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#ifndef RMF_PLUGINS_COMMON__READONLY_COMMON_HPP
+#define RMF_PLUGINS_COMMON__READONLY_COMMON_HPP
+
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp/logger.hpp>
+
+#include <memory>
+#include <unordered_map>
+#include <unordered_set>
+#include <mutex>
+#include <string>
+
+#include <Eigen/Geometry>
+
+#include <rmf_fleet_msgs/msg/robot_state.hpp>
+#include <building_map_msgs/msg/building_map.hpp>
+#include <building_map_msgs/msg/level.hpp>
+#include <building_map_msgs/msg/graph.hpp>
+
+namespace rmf_readonly_common {
+
+class ReadonlyCommon
+{
+public:
+
+  using BuildingMap = building_map_msgs::msg::BuildingMap;
+  using Level = building_map_msgs::msg::Level;
+  using Graph = building_map_msgs::msg::Graph;
+  using Location = rmf_fleet_msgs::msg::Location;
+  using Path = std::vector<Location>;
+
+  rclcpp::Node::SharedPtr ros_node;
+
+  void set_name(const std::string& name);
+  std::string get_name() const;
+  rclcpp::Logger logger();
+  template<typename SdfPtrT>
+  void read_sdf(SdfPtrT& sdf);
+  void init(rclcpp::Node::SharedPtr node);
+  void on_update(Eigen::Isometry3d& pose, double sim_time);
+
+private:
+  std::string _name = "caddy"; // Placeholder
+
+  // Updated in each on_update() call
+  Eigen::Isometry3d _pose;
+  double _sim_time = 0.0;
+
+  rclcpp::Publisher<rmf_fleet_msgs::msg::RobotState>::SharedPtr _robot_state_pub;
+  rclcpp::Subscription<BuildingMap>::SharedPtr _building_map_sub;
+
+  rmf_fleet_msgs::msg::RobotState _robot_state_msg;
+  rmf_fleet_msgs::msg::RobotMode _current_mode;
+
+  bool _found_level = false;
+  bool _found_graph = false;
+  bool _initialized_graph = false;
+  bool _initialized_start = false;
+
+  // Store cache of BuildingMap
+  Path _path;
+  Level _level;
+  Graph _graph;
+
+  std::string _level_name = "L1";
+  std::size_t _nav_graph_index = 1;
+  std::string _start_wp_name = "caddy";
+  // The number of waypoints to add to the predicted path. Minimum is 1.
+  std::size_t _lookahead = 1;
+  std::size_t _start_wp;
+  std::vector<std::size_t> _next_wp;
+
+  std::unordered_map<std::size_t,
+    std::unordered_set<std::size_t>> _neighbor_map;
+
+  double _last_update_time = 0.0;
+  double _update_threshold = 0.5; // Update every 0.5s
+  double _waypoint_threshold = 2.0;
+
+  bool _merge_lane = false;
+  double _lane_threshold = 0.2; // Meters
+
+  std::string _current_task_id;
+
+  std::mutex _graph_update_mutex;
+
+  void map_cb(const BuildingMap::SharedPtr msg);
+  void initialize_graph();
+  double compute_ds(
+    const Eigen::Isometry3d& pose,
+    const std::size_t& wp);
+  void initialize_start(const Eigen::Isometry3d& pose);
+  std::size_t get_next_waypoint(const std::size_t start_wp,
+    const Eigen::Vector3d& heading);
+  ReadonlyCommon::Path compute_path(
+    const Eigen::Isometry3d& pose);
+};
+
+template<typename SdfPtrT>
+void ReadonlyCommon::read_sdf(SdfPtrT& sdf)
+{
+  // Getting sdf elements
+  if (sdf->HasElement("level_name"))
+    _level_name = sdf->template Get<std::string>("level_name");
+  RCLCPP_INFO(logger(), "Setting level name to: " + _level_name);
+
+  if (sdf->HasElement("graph_index"))
+    _nav_graph_index = sdf->template Get<std::size_t>("graph_index");
+  RCLCPP_INFO(logger(), "Setting nav graph index: " + _nav_graph_index);
+
+  if (sdf->HasElement("spawn_waypoint"))
+    _start_wp_name = sdf->template Get<std::string>("spawn_waypoint");
+  RCLCPP_INFO(logger(), "Setting start wp name: " + _start_wp_name);
+
+  if (sdf->HasElement("look_ahead"))
+    _lookahead = sdf->template Get<std::size_t>("look_ahead");
+  _lookahead = _lookahead < 1 ? 1 : _lookahead;
+  _next_wp.resize(_lookahead);
+  _path.resize(_lookahead);
+  RCLCPP_INFO(logger(), "Setting lookahead: " + std::to_string(_lookahead));
+
+  if (sdf->HasElement("update_rate"))
+    _update_threshold = 1.0 / sdf->template Get<double>("update_rate");
+  RCLCPP_INFO(logger(),
+    "Setting update threshold: " + std::to_string(_update_threshold));
+
+  if (sdf->HasElement("waypoint_threshold"))
+    _waypoint_threshold = sdf->template Get<double>("waypoint_threshold");
+  RCLCPP_INFO(logger(),
+    "Setting waypoint threshold: " + std::to_string(_waypoint_threshold));
+
+  if (sdf->HasElement("merge_lane"))
+    _merge_lane = sdf->template Get<bool>("merge_lane");
+  RCLCPP_INFO(logger(), "Setting merge lane: " + std::to_string(_merge_lane));
+
+  if (sdf->HasElement("lane_threshold"))
+    _lane_threshold = sdf->template Get<double>("lane_threshold");
+  RCLCPP_INFO(logger(),
+    "Setting lane threshold: " + std::to_string(_lane_threshold));
+}
+
+} // namespace rmf_readonly_common
+
+#endif
